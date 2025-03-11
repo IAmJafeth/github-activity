e2c29ace79b82acf65ec2d168398750e039def18
Jafeth Garro <45522320+IAmJafeth@users.noreply.github.com>
2025-03-11 15:46:33 -0600

    Add API disclaimer to parser service documentation

--- src/parser_service.py
+++ src/parser_service.py
@@ -1,10 +1,12 @@
 import argparse
 
 def get_parser() -> argparse.ArgumentParser:
+    GITHUB_API_DISCLAIMER = "This API is not built to serve real-time use cases. Depending on the time of day, event latency can be anywhere from 30s to 6h."
+
     parser = argparse.ArgumentParser(
         prog='github-activity',
         description='Simple CLI tool to fetch Github Activity',
-        epilog='A Roadmap.sh Project | Created by: @IAmJafeth'
+        epilog=' API DISCLAIMER: ' + GITHUB_API_DISCLAIMER + ' | A Roadmap.sh Project | Created by: @IAmJafeth'
     )
 
     parser.add_argument('-v', '--version', action='version', version='%(prog)s 0.1.0')
